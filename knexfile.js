a772ee631b3a5f5e696e3147de6df92fc2877c1c
Aadarsh Kumar <aadarsh21@navgurukul.org>
2024-03-13 23:11:24 +0530

    fix issue

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const knexfile = {
     development: {
         client: 'pg',
+    searchPath: 'public',
         connection: {
             host: process.env.HOST || 'localhost',
             user: process.env.user || 'postgres',
