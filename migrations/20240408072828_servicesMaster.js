6667a8e51337d58d0b6c58de7715bc40eea6eca0
Aadarsh Kumar <aadarsh21@navgurukul.org>
2024-04-08 18:22:35 +0530

    add fee columns in services

--- migrations/20240408072828_servicesMaster.js
+++ migrations/20240408072828_servicesMaster.js
@@ -1,7 +1,9 @@
 exports.up = async (knex) => {
     await knex.schema.createTable('services_master_all', table => {
         table.increments('id'),
-        table.string('services').notNullable()
+        table.string('services').notNullable(),
+        table.string('consulting_fee'),
+        table.string('booking_fee')
     })
 };
 
